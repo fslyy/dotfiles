6c869a92a7deea832d704031f86a4f3e3abd14b7
fslyy <fefepenn@gmail.com>
2024-06-01 21:52:34 +0200

    nvim conf

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,10 @@ require("lazy_setup")
 local plugins = {}
 table.insert(plugins, require("plugins/vimtex"))
 table.insert(plugins, require("plugins/color"))
+table.insert(plugins, require("plugins/telescope"))
+table.insert(plugins, require("plugins/lualine"))
+table.insert(plugins, require("plugins/lsp"))
+table.insert(plugins, require("plugins/cmp"))
 
 require("lazy").setup(plugins)
 
