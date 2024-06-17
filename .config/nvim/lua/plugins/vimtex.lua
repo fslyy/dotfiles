6dcb6fa4ba48c014bb115768eaf7628d7bee094b
fslyy <fefepenn@gmail.com>
2024-06-17 15:46:55 +0200

    lualatex compiler added

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -1,6 +1,9 @@
-return {"lervag/vimtex", ft = {"tex", "latex"},                                
-                config = function()                                   
-                        vim.g.latex_view_general_viewer = "zathura"   
-			vim.g.vimtex_view_method = "zathura"          
-                end                                                   
-        } 
+return {
+	"lervag/vimtex",
+	ft = { "tex", "latex" },
+	config = function()
+		vim.g.latex_view_general_viewer = "zathura"
+		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_compiler_engine = "lualatex"
+	end,
+}
