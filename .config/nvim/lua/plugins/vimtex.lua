80cfe0eccbc0534c3b39b95bce29db8f0605acd3
fslyy <fefepenn@gmail.com>
2024-06-18 10:29:18 +0200

    fixed lualatex compiler for vimtex

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -4,6 +4,13 @@ return {
 	config = function()
 		vim.g.latex_view_general_viewer = "zathura"
 		vim.g.vimtex_view_method = "zathura"
-		vim.g.vimtex_compiler_engine = "lualatex"
+		vim.g.vimtex_compile_latexmk = {
+			options = {
+				"-shell-escape",
+				"-verbose",
+				"-interactions=nonstopmode",
+				"-synctex=1",
+			},
+		}
 	end,
 }
---
+++ .latexmkrc
@@ -0,0 +1,4 @@
+$pdflatex ="lualatex %O %S";
+#$pdf_mode = 4; # sets lualatex to default engine.
+#$dvi_mode = 0;
+#$postscript_mode = 0;
