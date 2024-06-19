0c9db9502c97a33459529dfa08c2497516dff2f7
fslyy <fefepenn@gmail.com>
2024-06-19 14:34:50 +0200

    live-grep shortcut added

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,4 +1,11 @@
 local km = vim.keymap
+
+-- nvim-tree
 local api = require("nvim-tree.api")
 
 km.set("n", "<C-t>", api.tree.toggle, { silent = true, desc = "toggle nvim-tree" })
+
+-- Telescope
+local scope = require("telescope.builtin")
+
+km.set("n", "<C-f>", scope.live_grep, { silent = true, desc = "live grep" })
