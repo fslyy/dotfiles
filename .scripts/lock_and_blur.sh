40c5a1b7e262e121e50c352bcf445aa7d750657b
fslyy <fefepenn@gmail.com>
2025-09-04 11:13:33 +0200

    changed lockscreen colors to hellwal

--- .scripts/lock_and_blur.sh
+++ .scripts/lock_and_blur.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
-icon="$HOME/Pictures/lock-icon-light.png"
 tmpbg='/tmp/screen.png'
+
+source ~/.cache/hellwal/variables.sh
 
 scrot -o "$tmpbg"
 
 
-PARAM=(--inside-color=ffffff00 --ring-color=d4be983e \
-       --time-color="#d4be9890" \
-       --date-color="#d4be9890" \
+PARAM=(--inside-color=ffffff00 --ring-color=$color0 \
+       --time-color=$color3 \
+       --date-color=$color3 \
        --clock \
        --time-str="%H:%M:%S"  \
        --date-str="%A, %m %Y" \
-       --line-color=ffffff00 --keyhl-color=a8998480 --ringver-color=00000000 \
-       --separator-color=d4be983e --insidever-color=a899841c \
-       --ringwrong-color=cc241d55 --insidewrong-color=a899841c)
+       --verif-color=$color3 --wrong-color=$color3 \
+       --line-color=00000000 --keyhl-color=$color2 --ringver-color=$color2 \
+       --separator-color=$color0 --insidever-color=00000000 \
+       --ringwrong-color=$color8 --insidewrong-color=00000000)
 
 magick "$tmpbg" -filter Gaussian -thumbnail 20% -sample 500% "$tmpbg"
 
-magick "$tmpbg" "$icon" -gravity center -composite "$tmpbg"
+magick "$tmpbg" -gravity center -composite "$tmpbg"
 
 i3lock "${PARAM[@]}" -i "$tmpbg"
 
